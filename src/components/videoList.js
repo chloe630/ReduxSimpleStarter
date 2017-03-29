468e0709a79353f37b23051968af13d062dbeafc
Chloe Choi <chloe.meowc@gmail.com>
2017-03-28 21:23:29 -0400

    yay!!!

---
+++ src/components/videoList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import VideoListItem from './videoListItem';
+
+
+const VideoList = (props) => {
+    // const videos = props.videos;
+    // console.log('props: ', props);
+    // console.log('videos: ',props.videos);
+    const videoItems = props.videos.map((video) => {
+        return (
+            <VideoListItem
+                onVideoSelect = { props.onVideoSelect }
+                key = { video. etag }
+                video = { video } />
+        );
+    });
+
+    return (
+        <ul className = "col-md-4 list-group">
+            { videoItems }
+        </ul>
+    );
+
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/searchBar';
+import VideoList from './components/videoList';
+import VideoDetail from './components/videoDetail';
+const API_KEY = 'AIzaSyDkzk_gATX0YIRmh_Ta_xwm-Gzm8-54Fes';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component{
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key : API_KEY, term : term }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange = { videoSearch } />
+                <VideoDetail video = { this.state.selectedVideo } />
+                <VideoList
+                    onVideoSelect = { selectedVideo => this.setState({ selectedVideo }) }
+                    videos = { this.state.videos } />
+           </div>
+        );
+    }
+
+}
+
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <App />,
+    document.getElementById('root')
+);
